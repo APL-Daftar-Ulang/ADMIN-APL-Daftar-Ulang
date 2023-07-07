75bdfc178ec4752ecb654eda0a9bd577abfec003
Adri-Bagas <abagaswitjaksono@gmail.com>
2023-07-07 13:49:09 +0700

    migrations

--- database/migrations/2023_07_07_013439_create_siswas_table.php
+++ database/migrations/2023_07_07_013439_create_siswas_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('siswas', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('user');
+            $table->foreignId('user_id')->constrained('users');
             $table->string('name');
             $table->string('NISN');
             $table->string('NIPD');
